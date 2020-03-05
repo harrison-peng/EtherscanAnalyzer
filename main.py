d2115cfe389cd9b84b1861c91397682743e518d1
Harrison Peng <harrison_peng@trendmicro.com>
2020-03-05 18:07:45 +0800

    add argsparser

--- main.py
+++ main.py
@@ -2,16 +2,50 @@ import os
 import requests
 import json
 import time
+import argparse
+import logging
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
 from subprocess import call
 from DB_Settings import *
 from Settings import *
 
+logging.basicConfig(
+    format='%(asctime)s [%(levelname)s]: %(message)s',
+    datefmt='%y-%m-%d %H:%M',
+    level=logging.INFO
+)
+
 client = MongoClient(MONGODB_URL, MONGODB_PORT, retryWrites=False)
 db = client[DB_NAME]
 db.authenticate(DB_USER, DB_PASSWORD)
-collection = db[COLLECTION_NAME]
+analyzed_collection = db[ANALYZED_COLLECTION]
+etherscan_collection = db[ETHERSCAN_COLLECTION]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--insert',dest='insert', help='insert contracts from Contract Library to DB', action='store_true')
+    parser.add_argument('-p', '--page',dest='page', help='the page to insert to DB', type=int, default=1)
+    parser.add_argument('-a', '--analyze',dest='analyze', help='analyze the contracts from DB', action='store_true')
+    parser.add_argument('-f', '--fix',dest='fix', help='fix the DB', action='store_true')
+    parser.add_argument('-g', '--getinfo',dest='getinfo', help='get the infomation', action='store_true')
+    parser.add_argument('-t', '--infotype',dest='infotype', help='the information type')
+
+    args = parser.parse_args()
+
+    if args.insert:
+        contract_library(args.page)
+    elif args.analyze:
+        analyze()
+    elif args.fix:
+        fix()
+    elif args.getinfo:
+        if args.infotype in ['constant', 'bound', 'unbound']:
+            get_info(args.infotype)
+        else:
+            logging.error('Please input -t [constant/bound/unbound]')
+    else:
+        logging.error('Must use an argument. --help for the detail')
 
 def etherscan():
     base_url = 'https://etherscan.io'
@@ -55,62 +89,58 @@ def get_bytecode_by_address(address):
             bytecode = code.text
     return bytecode
 
-def contract_library():
+def contract_library(start: int):
     from selenium import webdriver
-    # address = list()
     base_url = 'https://contract-library.com/'
+    count_exist = 0
 
-    for page in range(50,100):
-        if OS_ENV == 'macos':
-            driver = webdriver.Chrome('./chromedriver')
-        else:
-            driver = webdriver.Firefox(executable_path='./firefox-driver-linux')
-        url = base_url + '?p=' + str(page+1)
+    if OS_ENV == 'macos':
+        driver = webdriver.Chrome('./chromedriver')
+    else:
+        driver = webdriver.Firefox(executable_path='./firefox-driver-linux')
+
+    for page in range(start,1000):
+        
+        url = base_url + '?p=' + str(page)
         driver.get(url)
         time.sleep(3)
         table = driver.find_element_by_class_name('table-striped').find_element_by_tag_name('tbody')
         row = table.find_elements_by_tag_name('tr')
         for column in row:
+            if count_exist > 20:
+                driver.close()
+                return
+
             addr = column.find_elements_by_tag_name('td')[0].text
-            print(addr)
             byte_code = get_bytecode_by_address(addr)
-            insert_new_contract_to_db(addr, byte_code)
-            # address.append(addr)
-        driver.close()
+            existed = insert_new_contract_to_db(addr, byte_code)
+            if existed:
+                logging.info('P.%s %s: Exist' % (page, addr))
+                count_exist += 1
+            else:
+                count_exist = 0
+                logging.info('P.%s %s: Insert' % (page, addr))
+    driver.close()
 
-def insert_new_contract_to_db(address, bytecode):
-    contract = collection.find_one({'_id': address})
+def insert_new_contract_to_db(address: str, bytecode: str) -> bool:
+    contract = etherscan_collection.find_one({'_id': address})
     if not contract:
-        print('Insert')
-        new_item = {'_id': address, 'status': 'unchecked', 'gas_type': None, 'error': None, 'bytecode': bytecode}
-        resp = collection.insert_one(new_item)
+        new_item = {'_id': address, 'status': 'unchecked', 'bytecode': bytecode}
+        resp = etherscan_collection.insert_one(new_item)
+        return False
     else:
-        print('Exist')
+        return True
 
-def insert_check_list():
+def analyze():
     root_path = os.path.dirname(os.path.abspath(__file__))
-    for file_name in os.listdir(os.path.join(root_path, 'bytecode')):
-        address = file_name.split('.')[0]
-        contract = collection.find_one({'_id': address})
-        print('address:', address)
-        
-        with open(os.path.join(root_path, 'bytecode/%s' % file_name), 'r') as f:
-            bytecode = f.read()
-        print(bytecode)
-        if contract:
-            update_item = {'$set': {'bytecode': bytecode}}
-            resp = collection.update({'_id': address}, update_item)
-        else:
-            new_item = {'_id': address, 'check': 'unchecked', 'gas_type': '', 'bytecode': bytecode}
-            resp = collection.insert(new_item)
-
-def analyze(status):
-    root_path = os.path.dirname(os.path.abspath(__file__))
-    contract = collection.find_one({'status': status})
+    contract = etherscan_collection.find_one({'status': 'unchecked'})
     while contract:
         address = contract['_id']
+        new_item = {
+            '_id': address,
+            'bytecode': contract['bytecode']
+        }
         if contract['bytecode']:
-            check = contract['status']
             print('+--------------------------------------------+')
             print('|', address, '|')
             print('+--------------------------------------------+')
@@ -120,37 +150,56 @@ def analyze(status):
 
             call(['python', '%s/main.py' % SMARTCONTRACTCFG_PATH, '-b', '-r', '-code', file_path, '-o', ANALYSIS_RESULT_PATH])
             
-            if os.path.isfile('%s/%s/gas_type.txt' % (ANALYSIS_RESULT_PATH, address)):
-                with open('%s/%s/gas_type.txt' % (ANALYSIS_RESULT_PATH, address), 'r') as f:
-                    results = f.readlines()
-                    gas_type = results[0].strip()
-                    gas_formula = results[1].strip()
-                    max_gas = results[2].strip()
-                    ins_num = results[3].strip()
-                    node_num = results[4].strip()
-                    edge_num = results[5].strip()
+            insert_new = False
+            if os.path.isfile('%s/%s/info.json' % (ANALYSIS_RESULT_PATH, address)):
+                with open('%s/%s/info.json' % (ANALYSIS_RESULT_PATH, address), 'r') as f:
+                    info = json.load(f)
+                    gas_type = info['gas_type']
+                    gas_formula = info['gas_formula']
+                    max_gas = info['max_gas']
+                    ins_num = info['ins_num']
+                    node_num = info['node_num']
+                    edge_num = info['edge_num']
+                
+                check_query = {'gas_type': gas_type, 'instruction_number': ins_num, 'node_number': node_num, 'edge_number': edge_num, 'max_gas': max_gas}
+                contract = analyzed_collection.find_one(check_query)
 
-                update_value = {'$set': {'status': 'checked_2', 'gas_type': gas_type, 'gas_formula': gas_formula, 'max_gas': max_gas, 'instruction_number': ins_num, 'node_number': node_num, 'edge_number': edge_num}}
-            elif os.path.isfile('%s/%s/error.txt' % (ANALYSIS_RESULT_PATH, address)):
-                with open('%s/%s/error.txt' % (ANALYSIS_RESULT_PATH, address), 'r') as f:
-                    error = f.read()
-                update_value = {'$set': {'status': 'error_1', 'gas_type': '', 'gas_formula': '', 'max_gas': '', 'instruction_number': '', 'node_number': '', 'edge_number': '', 'error': error}}
+                if contract:
+                    update_value = {'$set': {'status': 'duplicate', 'reference_contract': contract['_id']}}
+                else:
+                    insert_new = True
+                    update_value = {'$set': {'status': 'checked'}}
+                    new_item['status'] = 'checked'
+                    new_item['gas_type'] = gas_type
+                    new_item['gas_formula'] = gas_formula
+                    new_item['max_gas'] = max_gas
+                    new_item['instruction_number'] = ins_num
+                    new_item['node_number'] = node_num
+                    new_item['edge_number'] = edge_num
+                    new_item['error'] = None
             else:
-                update_value = {'$set': {'status': 'error_1', 'gas_type': '', 'gas_formula': '', 'max_gas': '', 'instruction_number': '', 'node_number': '', 'edge_number': '', 'error': None}}
+                if os.path.isfile('%s/%s/error.txt' % (ANALYSIS_RESULT_PATH, address)):
+                    with open('%s/%s/error.txt' % (ANALYSIS_RESULT_PATH, address), 'r') as f:
+                        error = f.read()
+                else:
+                    error = None
+                update_value = {'$set': {'status': 'error', 'error_message': error}}
 
-            collection.update_one({'_id': address}, update_value)
+            etherscan_collection.update_one({'_id': address}, update_value)
+            if insert_new:
+                analyzed_collection.insert_one(new_item)
 
             call(['rm', file_path])
         else:
-            print('No bytecode: %s' % address)
+            logging.info('No bytecode: %s' % address)
             update_value = {'$set': {'status': 'no_bytecode'}}
-            collection.update_one({'_id': address}, update_value)
+            etherscan_collection.update_one({'_id': address}, update_value)
         
-        contract = collection.find_one({'status': status})
+        contract = etherscan_collection.find_one({'status': status})
         print('\n')
 
-def find_constant_info():
-    contract_list = collection.find({'gas_type': 'constant'}, no_cursor_timeout=True)
+def get_info(contract_type):
+    contract_list = analyzed_collection.find({'gas_type': contract_type}, no_cursor_timeout=True)
     count = 0
     gas_min = 10000
     gas_max = 0
@@ -164,41 +213,27 @@ def find_constant_info():
         if count % 50 == 0:
             print(count)
     contract_list.close()
-    print('Constant contract:', count)
-    print('Max gas:', gas_max)
-    print('Min gas:', gas_min)
-    print('Average gas:', gas_sum/count)
-
-def fix_db():
-    contract = collection.find_one({'status': 'loop_error'})
-    while contract:
-        address = contract['_id']
-        update_value = {'$set': {'status': 'unchecked', 'error': None}}
-        collection.update_one({'_id': address}, update_value)
-        print(address)
-        contract = collection.find_one({'status': 'loop_error'})
+    logging.info('#Contract:', count)
+    logging.info('Max gas:', gas_max)
+    logging.info('Min gas:', gas_min)
+    logging.info('Average gas:', gas_sum/count)
 
 def fix():
-    query = {
-        "status": "unchecked",
-        "gas_type": "unbound",
-        "edge_number": "197",
-        "instruction_number": "2205",
-        "max_gas": "2397+3*BV2Int(Isize_344)+1228*BV2Int(loop_1830)",
-        "node_number": "192"
-    }
-    contract = collection.find_one(query)
-    while contract:
+    query = {'status': 'checked'}
+    contract_list = analyzed_collection.find(query, no_cursor_timeout=True)
+    for contract in contract_list:
+        max_gas = int(contract['max_gas']) if contract['max_gas'].isdigit() else contract['max_gas']
         address = contract['_id']
-        update_value = {'$set': {'status': 'checked_2', 'gas_type': 'bound', 'gas_formula': '33009', 'max_gas': '33009', 'instruction_number': '2244', 'node_number': '195', 'edge_number': '217'}}
-        collection.update_one({'_id': address}, update_value)
-        print(address)
-        contract = collection.find_one(query)
-
+        update_value = {
+            '$set': {
+                'instruction_number': int(contract['instruction_number']),
+                'max_gas': max_gas,
+                'node_number': int(contract['node_number']),
+                'edge_number': int(contract['edge_number'])
+            }
+        }
+        analyzed_collection.update_one({'_id': address}, update_value)
+        logging.info(address, update_value)
 
 if __name__ == '__main__':
-	# contract_library()
-    analyze('unchecked')
-    # fix_db()
-    # find_constant_info()
-    # fix()
+    main()
